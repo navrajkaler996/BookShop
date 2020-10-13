0193a7dff6868b3a7cae6e8071d64ce0a7e33b59
navrajkaler01996 <navrajskkaler@gmail.com>
2020-10-14 01:27:01 +0530

    First course done!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
+import HomeScreen from "./screens/HomeScreen.js";
+import ProductScreen from "./screens/ProductScreen.js";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header/>
       <main className="py-3">
         <Container>
-          <h1>Welcome to ProShop</h1>
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
-      <Footer/>
-      
-    </>
+      <Footer/> 
+    </Router>
   );
 };
 
