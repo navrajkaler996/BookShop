25c7e44676601960e33a05c9358d258eeae5f052
navrajkaler01996 <navrajskkaler@gmail.com>
2021-01-13 14:01:07 +0530

    next

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,9 @@
 import express from "express"
 const router = express.Router()
-import { authUser, getUserProfile } from "../controllers/userController.js"
+import { authUser, registerUser, getUserProfile, updateUserProfile } from "../controllers/userController.js"
 import { protect } from "../middleware/authMiddleware.js"
 
 router.post("/login", authUser)
-router.route("/profile").get(protect, getUserProfile)
-
+router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile)
+router.route("/").post(registerUser)
 export default router
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,13 @@ import Footer from "./components/Footer.js";
 import HomeScreen from "./screens/HomeScreen.js";
 import ProductScreen from "./screens/ProductScreen.js";
 import CartScreen from "./screens/CartScreen.js";
+import LoginScreen from "./screens/LoginScreen.js";
+import RegisterScreen from "./screens/RegisterScreen.js";
+import ProfileScreen from "./screens/ProfileScreen.js";
+import ShippingScreen from "./screens/ShippingScreen.js";
+import PaymentScreen from "./screens/PaymentScreen.js";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderScreen";
 
 const App = () => {
   return (
@@ -16,6 +23,13 @@ const App = () => {
           <Route path="/" component={HomeScreen} exact />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/login" component={LoginScreen} />
+          <Route path="/register" component={RegisterScreen} />
+          <Route path="/profile" component={ProfileScreen} />
+          <Route path="/shipping" component={ShippingScreen} />
+          <Route path="/payment" component={PaymentScreen} />
+          <Route path="/placeorder" component={PlaceOrderScreen} />
+          <Route path="/order/:id" component={OrderScreen} />
         </Container>
       </main>
       <Footer/> 
