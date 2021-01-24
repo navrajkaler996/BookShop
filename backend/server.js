a23a959a54e1d3d991df3eb561749dba7acb7c05
navrajkaler01996 <navrajskkaler@gmail.com>
2021-01-24 20:45:24 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
 import connectDB from "./config/db.js";
 import colors from "colors";
 import productRoutes from "./routes/productRoutes.js";
@@ -16,6 +17,9 @@ connectDB();
 
 const app = express();
 
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
+}
 app.use(express.json());
 
 app.get("/", (req, res) => {
---
+++ frontend/build/precache-manifest.cea2310281b5f5825f73a3f0bd4ccb71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "981bdae878ae416c20661b70f9b9a2db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ed87748017a01935991",
+    "url": "/static/css/main.c97053eb.chunk.css"
+  },
+  {
+    "revision": "d3ec8e28a34fcc7479f6",
+    "url": "/static/js/2.459f6d52.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.459f6d52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ed87748017a01935991",
+    "url": "/static/js/main.66452e35.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
